b2e447306c18e127a2f73c38c2fa5fab748eec78
SAI PRASAD <sp12961369@gmail.com>
2025-09-21 18:42:50 +0530

    Basics

---
+++ Day_1/basics.py
@@ -0,0 +1,28 @@
+### Python Refresher
+
+# Variables
+x = 10
+y = 20
+z = x + y
+print(z)
+
+# Strings
+name = "Sai Prasad"
+print (f"Hello {name}")
+
+# Operators
+
+# Logical Operators
+a = True
+b = False
+c = True and False
+print(c)
+
+# Membership Operators
+a = [1, 2, 3]
+print(1 in a)
+
+# For Loops
+for i in range(1,5):
+    print(i)
+
